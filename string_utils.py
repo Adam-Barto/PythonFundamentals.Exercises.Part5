6de765663a7c476b8dabcb1c6ce8c45a3143a98d
Adam-Barto <adam.jbarto@gmail.com>
2023-03-14 19:06:01 -0400

    String Utils finished

--- string_utils.py
+++ string_utils.py
@@ -18,8 +18,8 @@ def last_char(str_in: str) -> str:
     """
     Given a string parameter, this function should return the last letter of the parameter..
     """
-    return str_in[str_len(str_in)-1]
-     #pass  # remove pass statement and implement me
+    return str_in[str_len(str_in) - 1]
+    # pass  # remove pass statement and implement me
 
 
 def input_has_substring(str_in: str, sub_str_in: str) -> bool:
@@ -27,7 +27,6 @@ def input_has_substring(str_in: str, sub_str_in: str) -> bool:
     This function determines if the substring exists within the string. Returns True or False.
     """
     return str_in.find(sub_str_in) != -1
-     #pass  # remove pass statement and implement me
 
 
 def substring(str_in: str, start: int, stop: int) -> str:
@@ -39,7 +38,7 @@ def substring(str_in: str, start: int, stop: int) -> str:
     start -- starting position of the input parameter to start the substring (inclusive)
     stop -- stopping position of the input parameter to stop the substring (exclusive)
     """
-    pass  # remove pass statement and implement me
+    return str_in[start: stop]
 
 
 def opposite_case(str_in: str) -> str:
@@ -48,4 +47,5 @@ def opposite_case(str_in: str) -> str:
     Example: 
     When input = "Python" the function returns "pYTHON"
     """
-    pass  # remove pass statement and implement me
+    return str_in.swapcase()
+
